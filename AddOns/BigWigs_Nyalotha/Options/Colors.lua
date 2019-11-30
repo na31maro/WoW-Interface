a4e6434f57105666f4f025678d172a5075c7b820
Nick Marotta <na31maro@siena.edu>
2019-11-29 20:01:31 -0500

    11/29/2019

---
+++ AddOns/BigWigs_Nyalotha/Options/Colors.lua
@@ -0,0 +1,10 @@
+
+BigWigs:AddColors("Shad'har the Insatiable", {
+	[306942] = "cyan",
+	[307358] = {"blue","yellow"},
+	[307471] = {"blue","purple"},
+	[307472] = {"blue","cyan"},
+	[312528] = "yellow",
+	[312529] = "yellow",
+	[312530] = "yellow",
+})
---
+++ AddOns/BigWigs_Nyalotha/Options/Sounds.lua
@@ -0,0 +1,10 @@
+
+BigWigs:AddSounds("Shad'har the Insatiable", {
+	[306942] = "info",
+	[307358] = "info",
+	[307471] = "alarm",
+	[307472] = "info",
+	[312528] = "alert",
+	[312529] = "alert",
+	[312530] = "alert",
+})
